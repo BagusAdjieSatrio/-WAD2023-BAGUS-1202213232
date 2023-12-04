071cea38c367fc98133d480f3a85cbf12b49a878
BagusAdjieSatrio <bagusadjie15@gmail.com>
2023-12-04 12:05:57 +0700

    Modul 5

---
+++ Modul5-Bagus/app/Http/Controllers/ShowroomController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ShowroomController extends Controller
+{
+    public function create() {
+        return view('showroom.create');
+    }
+
+    public function store(request $request){
+    $data = $request->all();
+
+    Showroom :: create([
+        'id' -> $data['id'],
+        'nama_mobil' -> $data['nama mobil'],
+        'brand_mobil' -> $data['brand mobil'],
+        'warna_mobil' -> $data['warna mobil'],
+        'tipe_mobil'-> $data['tipe mobil'],
+        'harga_mobil'-> $data['harga mobil'],
+    ]);
+    return redirect(route('Showroom.index'));
+    }
+    //
+
+    public function index(){
+        $Showroom = Showroom:: all();
+        
+        return view ('Showroom/index', compact('Showroom'));
+    }
+}
